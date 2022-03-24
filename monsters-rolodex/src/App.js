417a43d434893948481850eb8cd4b9565317eb24
shermaine <shermaine.lim.si.hui@gmail.com>
2022-03-24 11:45:38 +0800

    setState

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -18,7 +18,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hi {this.state.name}</p>
-          <button>change name</button>
+          <button
+            onClick={() => {
+              this.setState({ name: "Andre" });
+            }}
+          >
+            change name
+          </button>
         </header>
       </div>
     );
