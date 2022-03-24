17116e93cb09735781d86e5102e5c3f7d49a80fe
shermaine <shermaine.lim.si.hui@gmail.com>
2022-03-24 11:30:06 +0800

    class component and state

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //always a json obj
+      //give a key value pair
+
+      name: "Anx",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name}</p>
+          <button>change name</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
