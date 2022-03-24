2419af44672804f0faf367dd6d32b484f400a28f
shermaine <shermaine.lim.si.hui@gmail.com>
2022-03-24 13:50:29 +0800

    search box component and filter func
    Input field and onChange

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -24,6 +24,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name
+                .toLowerCase()
+                .includes(event.target.value.toLowerCase());
+            });
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
