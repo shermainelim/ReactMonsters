4004b1ce670986822fea45ab95ad9b8a0f958958
shermaine <shermaine.lim.si.hui@gmail.com>
2022-03-25 22:53:30 +0800

    robohash org and card styling

--- monsters-rolodex/src/components/card-list/cardList.jsx
+++ monsters-rolodex/src/components/card-list/cardList.jsx
@@ -1,13 +1,25 @@
 import { Component } from "react";
+import "./cardList.css";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
+
     return (
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+            <div className="card-container" key={id}>
+              <img
+                alt={`monsters ${name}`}
+                src={`https://robohash.org/${id}?set=set2`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
